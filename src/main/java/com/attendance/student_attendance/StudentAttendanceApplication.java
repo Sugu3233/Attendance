e2797747262df47cae5f32d62d334d66b0594c0b
Sugumar.C <119380933+Sugu3233@users.noreply.github.com>
2026-06-18 10:59:51 +0530

    Files Uploaded
    Initial Commit

---
+++ src/main/java/com/attendance/student_attendance/StudentAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.student_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attendance/student_attendance/StudentAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.student_attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
